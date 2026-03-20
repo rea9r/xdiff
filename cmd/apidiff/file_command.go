deb9ef5c3169a6b75ff185cc397f6b1dd6bb7459
takuya.akamatsu <takuya.akamatsu@linecorp.com>
2026-03-20 21:35:09 +0900

    refactor: rename and split CLI command files for clarity

---
+++ cmd/apidiff/file_command.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rea9r/apidiff/internal/app"
+	"github.com/spf13/cobra"
+)
+
+func runFileCompare(common *commonFlagValues, exitCode *int) func(*cobra.Command, []string) error {
+	return func(_ *cobra.Command, positionalArgs []string) error {
+		opts := app.Options{
+			Format:       common.format,
+			IgnorePaths:  append([]string(nil), common.ignorePaths...),
+			OnlyBreaking: common.onlyBreaking,
+			OldPath:      positionalArgs[0],
+			NewPath:      positionalArgs[1],
+		}
+
+		code, out, err := app.RunWithOptions(opts)
+		if writeErr := writeOutput(out); writeErr != nil {
+			return asRunError(2, fmt.Errorf("failed to write stdout: %w", writeErr))
+		}
+		if err != nil {
+			return asRunError(code, err)
+		}
+
+		*exitCode = code
+		return nil
+	}
+}
--- cmd/apidiff/root_command.go
+++ cmd/apidiff/root_command.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/rea9r/apidiff/internal/app"
-	"github.com/spf13/cobra"
-)
+import "github.com/spf13/cobra"
 
 func newRootCommand(exitCode *int) *cobra.Command {
 	commonFlags := newCommonFlags()
@@ -22,27 +17,4 @@ func newRootCommand(exitCode *int) *cobra.Command {
 	bindCommonFlags(root.Flags(), commonFlags)
 	root.AddCommand(newURLCommand(commonFlags, exitCode))
 	return root
-}
-
-func runFileCompare(common *commonFlagValues, exitCode *int) func(*cobra.Command, []string) error {
-	return func(_ *cobra.Command, positionalArgs []string) error {
-		opts := app.Options{
-			Format:       common.format,
-			IgnorePaths:  append([]string(nil), common.ignorePaths...),
-			OnlyBreaking: common.onlyBreaking,
-			OldPath:      positionalArgs[0],
-			NewPath:      positionalArgs[1],
-		}
-
-		code, out, err := app.RunWithOptions(opts)
-		if writeErr := writeOutput(out); writeErr != nil {
-			return asRunError(2, fmt.Errorf("failed to write stdout: %w", writeErr))
-		}
-		if err != nil {
-			return asRunError(code, err)
-		}
-
-		*exitCode = code
-		return nil
-	}
 }
